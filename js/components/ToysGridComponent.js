acf2cb00f026b54214e18b3154bb20f45b6df279
adelaidele <tartilaitee@gmail.com>
2021-11-14 18:05:42 +0200

    6: component fetch toys

--- js/components/ToysGridComponent.js
+++ js/components/ToysGridComponent.js
@@ -1,10 +1,35 @@
 class ToysGridComponent {
     constructor(){
+        this.state = {
+            loading: false,
+            Toys: []
+          }
         this.init();
+    }
+    fetchToys = () => API.fetchToys(this.saveToys, alert);
+
+    saveToys = (toys) => {
+        this.state.toys = toys;
+        this.state.loading = false;
+
+        this.render();
     }
 
     init = () => {;
+        this.state.loading = true;
+        this.fetchToys();
         this.htmlElement = document.createElement("div");
         this.htmlElement.innerHTML = 'testas';
+
+        this.render();
+    }
+    
+    render = () => {
+        const { loading, toys } = this.state;
+        if (loading) {
+          this.htmlElement.innerHTML = 'siunciama.';
+        } else {
+          this.htmlElement.innerHTML = 'parsiusta.';
+        }  
     }
 }
