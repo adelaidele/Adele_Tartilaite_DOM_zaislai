bcb9041111f8d5d4a147cf2d127f57b1883f3353
adelaidele <tartilaitee@gmail.com>
2021-11-14 17:08:36 +0200

    4: komponentu pasiruosimas/testas

---
+++ js/components/ToysGridComponent.js
@@ -0,0 +1,10 @@
+class ToysGridComponent {
+    constructor(){
+        this.init();
+    }
+
+    init = () => {;
+        this.htmlElement = document.createElement("div");
+        this.htmlElement.innerHTML = 'testas';
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,5 @@
+const rootElement = document.querySelector("#root");
+
+const toysGridComponent = new ToysGridComponent();
+
+rootElement.appendChild(toysGridComponent.htmlElement);
